5ee1e5b9b31b91ac76369837f20fca137d1a1d80
EnivaldoPinheiro <56415884+EnivaldoPinheiro@users.noreply.github.com>
2019-12-08 21:06:34 -0300

    Aprendoemcasa

--- main.cs
+++ main.cs
@@ -1,15 +1,31 @@
 using System;
+using System.Collections.Generic;
+
 
 class MainClass {
-  public static void Main (string[] args) {
-    Banco esp = new Banco();
-    esp.setNome ("Enivaldo");
-    esp.setCCorrente(0001);
-    esp.setSaldo(10000);
-
-    Console.WriteLine("Entre com o nome do correntista "  + esp.getNome());
+  public static void Main (string[] args) {   
+  
+    //Declaração da lista
+    List<string> lista = new List<string>();
+             
+    string opcao = "1";
  
-    Cliente nm1 = new Cliente("fisico","Pinheiro ","000.111.222.00");
+    while(opcao == "1")
+    {
+        Console.WriteLine("Digite um nome para inserir na lista:");
+        string nome = Console.ReadLine();
+        //Adiciona o item à lista
+        lista.Add(nome);
+        Console.WriteLine("Deseja inserir outro nome? 1-SIM | 2-NÃO");
+        opcao = Console.ReadLine();
+    }
+    //Ordena a lista 
+    lista.Sort();
+    Console.WriteLine("A lista tem " + lista.Count + " itens:");
+    //Imprime cada item da lista
+    lista.ForEach(i => Console.WriteLine(i));
+    
     
   }
+
 }
