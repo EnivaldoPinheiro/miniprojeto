7230afbbdb57a56ba8ff9fed8ea0dda3089414e9
EnivaldoPinheiro <56415884+EnivaldoPinheiro@users.noreply.github.com>
2019-12-15 13:11:43 -0300

    Add files via upload

--- main.cs
+++ main.cs
@@ -1,23 +1,24 @@
+//Crie um programa em C# que calcule o resto da divisão inteira entre dois números por meio de sucessivas subtrações.
 using System;
-using System.Collections.Generic;
-
-
 
 class MainClass {
   public static void Main (string[] args) {
+    Console.WriteLine("Digite o dividendo: ");
+    int dividendo = int.Parse( Console.ReadLine() );
+    Console.WriteLine("Digite o divisor: ");
+    int divisor = int.Parse( Console.ReadLine() );
+    Console.WriteLine("\n O resto da divisão é : "+ recursivo(divisor, dividendo));
+  }
 
-    Cartao meuCard = new Cartao(350);
-    Maquina maquininha = new Maquina("CINEMA");
-    
-    if (maquininha.inserirCartao(45, meuCard)){
-      Console.WriteLine("COMPRA REALIZADA");
+  public static int recursivo(int divisor, int dividendo){
+    if(dividendo < divisor){
+      Console.Write( "Resultado: "+dividendo );
+      return dividendo;
     }
     else{
-      Console.WriteLine("COMPRA NEGADA");
+      Console.Write( String.Format("{0} - {1} = {2} | ", dividendo, divisor, (dividendo-divisor) ) );
+      return recursivo(divisor, dividendo-divisor);
     }
-  
-    
-   
-
+      
   }
 }
