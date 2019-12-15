10428c44e1cbe563ce4d81b734a094b2225d1736
EnivaldoPinheiro <56415884+EnivaldoPinheiro@users.noreply.github.com>
2019-12-15 13:06:33 -0300

    Cartão

--- main.cs
+++ main.cs
@@ -2,30 +2,22 @@ using System;
 using System.Collections.Generic;
 
 
-class MainClass {
-  public static void Main (string[] args) {   
-  
-    //Declaração da lista
-    List<string> lista = new List<string>();
-             
-    string opcao = "1";
- 
-    while(opcao == "1")
-    {
-        Console.WriteLine("Digite um nome para inserir na lista:");
-        string nome = Console.ReadLine();
-        //Adiciona o item à lista
-        lista.Add(nome);
-        Console.WriteLine("Deseja inserir outro nome? 1-SIM | 2-NÃO");
-        opcao = Console.ReadLine();
-    }
-    //Ordena a lista 
-    lista.Sort();
-    Console.WriteLine("A lista tem " + lista.Count + " itens:");
-    //Imprime cada item da lista
-    lista.ForEach(i => Console.WriteLine(i));
-    
-    
-  }
 
+class MainClass {
+  public static void Main (string[] args) {
+
+    Cartao meuCard = new Cartao(350);
+    Maquina maquininha = new Maquina("CINEMA");
+    
+    if (maquininha.inserirCartao(45, meuCard)){
+      Console.WriteLine("COMPRA REALIZADA");
+    }
+    else{
+      Console.WriteLine("COMPRA NEGADA");
+    }
+  
+    
+   
+
+  }
 }
